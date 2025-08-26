239931d03cfeee5b516c7479080fbcb3bae4868c
samueldiamond <49480731+samueldiamond@users.noreply.github.com>
2025-08-26 19:33:22 +0100

    new post

--- js/posts.js
+++ js/posts.js
@@ -3,5 +3,10 @@
 // Posts are automatically sorted by date (newest first)
 
 let blogPosts = [
-    // Your posts will appear here
+    {
+        "id": "1756233184572",
+        "title": "primrose hill",
+        "date": "2025-08-26",
+        "content": "I walked from Barbican to Baker's Street and then up through Regent's Park to Primrose Hill and then down Regent's Canal to King's Cross and then down through Farringdon and back to Barbican. I thought it would clear my head and it did a little but mostly it just made my knees sore. I saw a manic child go chaos mode and jump fully clothed into Regent's Park Boating lake, it was amazing."
+    }
 ];
