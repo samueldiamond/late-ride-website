5db404d860d979132a17b309e750d1ce9a646e54
samueldiamond <49480731+samueldiamond@users.noreply.github.com>
2025-08-27 21:55:09 +0100

    new blog

--- js/posts.js
+++ js/posts.js
@@ -4,6 +4,12 @@
 
 let blogPosts = [
     {
+        "id": "1756328075570",
+        "title": "Bonds",
+        "date": "2025-08-27",
+        "content": "I think a lot of people are very confused and don’t know what is good and what is bad. I count myself among this group.\n\nI think it’s bad that the money supply is in the hands of a small coterie of unelected technocrats but also when DJT goes crazy and starts firing people because they won’t lower interest rates or whatever I think that’s bad too. I think it’s bad when Liz Truss tries to radically lower taxes but I think as someone in the highest democratic office she should be able to do this without unelected bond investors telling her she can’t. And this is truer today when bond yields are about to break new highs in Britain and in France, and there is nothing good here, there rarely is."
+    },
+    {
         "id": "1756233184572",
         "title": "primrose hill",
         "date": "2025-08-26",
